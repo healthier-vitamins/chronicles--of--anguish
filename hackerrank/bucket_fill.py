d0cacf025dd71381c0ef30c68ecac63ec2776152
healthier-vitamins <88876449+healthier-vitamins@users.noreply.github.com>
2022-08-10 12:09:14 +0800

    bucket fill update

--- hackerrank/bucket_fill.py
+++ hackerrank/bucket_fill.py
@@ -18,13 +18,26 @@ def strokesRequired(picture):
             for j, w in enumerate(h):
                 max_length = len(h)
                 if j + 1 != max_length:
-                    if h[j + 1] == h[j] and w == letters[i]:
+                    if h[j + 1] == h[j] and w == letters[p]:
                         is_filled[i][j] = 1
                         is_filled[i][j + 1] = 1
                 else:
-                    if h[j] == h[max_length - 1] and w == letters[i]:
+                    if h[j] == h[max_length - 1] and w == letters[p]:
                         is_filled[i][max_length - 1] = 1
                         is_filled[i][j] = 1
+        # swap j and i
+        for j in range(0, len(picture[0])):
+            for i, h in enumerate(picture):
+                if i + 1 != len(picture):
+                    if picture[i + 1][j] == picture[i][j] and picture[i][j] == letters[p]:
+                        is_filled[i][j] = 1
+                        is_filled[i + 1][j] = 1
+                else:
+                    if picture[i][j] == picture[len(picture) - 1][j] and picture[i][j] == letters[p]:
+                        is_filled[i][j] = 1
+                        is_filled[len(picture) - 1][j] = 1
         p += 1
-    return is_filled
+        no_of_fill += 1
+    return is_filled, no_of_fill
 print(strokesRequired(picture))
+
