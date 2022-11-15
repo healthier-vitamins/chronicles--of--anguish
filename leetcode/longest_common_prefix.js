21acdf2a21a20764bf1b3559ebce520ce42d794e
healthy-vitamins <cheeaudrian97@gmail.com>
2022-11-16 02:02:49 +0800

    Update longest_common_prefix.js

--- leetcode/longest_common_prefix.js
+++ leetcode/longest_common_prefix.js
@@ -2,7 +2,7 @@ var longestCommonPrefix = function (strs) {
   if (strs.length === 0) return "";
 
   var prefix = strs[0];
-  for (let i = 0; i < strs.length; i++) {
+  for (let i = 1; i < strs.length; i++) {
     while (strs[i].indexOf(prefix) !== 0) {
       prefix = prefix.substring(0, prefix.length - 1);
     }
